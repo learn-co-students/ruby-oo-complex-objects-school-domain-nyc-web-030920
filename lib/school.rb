9a5754f75f4be6d0b37aec61ae3e982673223445
github username <github email address>
2020-03-10 17:30:53 -0400

    commit

--- lib/school.rb
+++ lib/school.rb
@@ -1,27 +1,22 @@
 # code here!
 def School 
+    attr_accessor :name, :roster 
+
     def initialize(name)
         @name = name 
         @roster = {}
     end 
-    def roster 
-        @roster 
-    end 
     def add_student(name, grade) 
-        if roster[grade]
-            roster[grade] << name 
-        else
-            roster[grade] = [] 
-            roster[grade] << name 
-        end 
+        roster[grade] ||= []
         roster[grade] << name 
-    end 
     def grade(grade)
         roster[grade]
     end 
     def sort 
-        @roster.each do |k, v| 
-            v.sort!
+        sorted = {}
+        roster.each do |grade, students| 
+            sorted[grade] = students.sort 
         end 
+        sorted 
     end 
 end 
