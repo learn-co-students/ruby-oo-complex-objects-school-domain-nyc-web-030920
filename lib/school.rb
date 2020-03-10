393caaefe4772b4ac6feec5bc43cb5dcf0bd83c8
github username <github email address>
2020-03-10 17:37:17 -0400

    commit

--- lib/school.rb
+++ lib/school.rb
@@ -6,8 +6,12 @@ def School
         @roster = {}
     end 
     def add_student(name, grade) 
-        roster[grade] ||= []
-        roster[grade] << name 
+        if !(@roster.include?(grade))
+            @roster[grade] = [name]
+        else
+            @roster[grade] << name 
+        end 
+    end 
     def grade(grade)
         roster[grade]
     end 
